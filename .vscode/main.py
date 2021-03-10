8b2941c56d5b906ec2b7fa3c68d72c064569758d
ASUS <eb.mehrdad@gmail.com>
2021-03-10 10:23:54 +0330

    second commit

--- .vscode/main.py
+++ .vscode/main.py
@@ -19,4 +19,11 @@ for _ in range(2):
 
 
 def calculator_score(cards):
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+
     return sum(cards)
